cd4085fdc1bfe5df36517158e870e618c7bfb494
michael-mcmasters <jmcmasters411@gmail.com>
2020-11-06 21:09:21 -0500

    Use Javascript to enable/disable Scientific calculator

--- scientific-calculator.js
+++ scientific-calculator.js
@@ -1,43 +1,63 @@
-console.log("Initiating Script");
+let scientificCalculatorActive = false;
 
-let classes = document.getElementsByClassName("row");
-for (let row = 0; row < classes.length; row++) {
-  let newHTML = getScientificButtons(row);
-  let existingHTML = classes[row].innerHTML;
-  classes[row].innerHTML = newHTML + existingHTML;
+let tag = document.querySelector("a");
+  a.addEventListener("click", () => {
+    scientificCalculatorActive = !scientificCalculatorActive;
+    if (scientificCalculatorActive) {
+      enableScientificCalculator();
+    } else {
+      disableScientificCalculator();
+    }
+});
+
+function enableScientificCalculator() {
+  let rows = document.getElementsByClassName("row");
+  for (let row = 0; row < rows.length; row++) {
+    let newHTML = getScientificButtons(row);
+    let existingHTML = rows[row].innerHTML;
+    rows[row].innerHTML = newHTML + existingHTML;
+  }  
+}
+
+function disableScientificCalculator() {
+  let scientificButtons = document.querySelectorAll(".scientific");
+  console.log(scientificButtons);
+  for (let row = 0; row < scientificButtons.length; row++) {
+    scientificButtons[row].remove();
+  }  
 }
 
 function getScientificButtons(row) {
   switch(row) {
     case 0:
       return `
-        <button class="btn-light-gray">Rad</button>
-        <button class="btn-light-gray">Deg</button>
-        <button class="btn-light-gray">x!</button>
+        <button class="scientific btn-light-gray">Rad</button>
+        <button class="scientific btn-light-gray">Deg</button>
+        <button class="scientific btn-light-gray">x!</button>
       `
     case 1:
       return `
-        <button class="btn-light-gray">Inv</button>
-        <button class="btn-light-gray">sin</button>
-        <button class="btn-light-gray">In</button>
+        <button class="scientific btn-light-gray">Inv</button>
+        <button class="scientific btn-light-gray">sin</button>
+        <button class="scientific btn-light-gray">In</button>
       `
     case 2:
       return `
-        <button class="btn-light-gray">Pie</button>
-        <button class="btn-light-gray">cos</button>
-        <button class="btn-light-gray">log</button>
+        <button class="scientific btn-light-gray">Pie</button>
+        <button class="scientific btn-light-gray">cos</button>
+        <button class="scientific btn-light-gray">log</button>
       `
     case 3:
       return `
-        <button class="btn-light-gray">e</button>
-        <button class="btn-light-gray">tan</button>
-        <button class="btn-light-gray">check</button>
+        <button class="scientific btn-light-gray">e</button>
+        <button class="scientific btn-light-gray">tan</button>
+        <button class="scientific btn-light-gray">check</button>
       `
     case 4:
       return `
-        <button class="btn-light-gray">Ans</button>
-        <button class="btn-light-gray">EXP</button>
-        <button class="btn-light-gray">X^y</button>
+        <button class="scientific btn-light-gray">Ans</button>
+        <button class="scientific btn-light-gray">EXP</button>
+        <button class="scientific btn-light-gray">X^y</button>
       `
   }
 }
