1f0030d2b111529b4ea434cb7b2037f9719e878c
michael-mcmasters <jmcmasters411@gmail.com>
2020-11-09 15:45:45 -0500

    Make Scientific buttons responsive with Javascript. Add/remove correct buttons with click listener

--- scientific-calculator-enabler.js
+++ scientific-calculator-enabler.js
@@ -1,21 +1,34 @@
-let scientificButtonsEnabled = false;
-let standardButton = document.getElementById("standard-button");
-let scientificButton = document.getElementById("scientific-button");
+// By default, scientific buttons have a class set to display: none.
+// This script enables/disables them depending on the button pressed and the screen width.
 
-standardButton.addEventListener("click", () => {
-  toggleScientificButtons(false);
-});
+let scientificButtonsEnabled = false;
+let scientificButton = document.getElementById("scientific-button");
+let standardButton = document.getElementById("standard-button");
 
 scientificButton.addEventListener("click", () => {
-  toggleScientificButtons(true);
+  enableScientificButtons();
+  scientificButtonsEnabled = true;
 });
 
-function toggleScientificButtons(enable) {
+standardButton.addEventListener("click", () => {
+  disableScientificButtons();
+  scientificButtonsEnabled = false;
+});
+
+// Responsive: Moves buttons to the left or bottom of calculator depending on screen width.
+window.addEventListener("resize", () => {
+  if (scientificButtonsEnabled) {
+    enableScientificButtons();
+  }
+});
+
+// Enables buttons on the left or bottom depending on screen width.
+function enableScientificButtons() {
+  disableScientificButtons(); // Edge Case: Disable any existing buttons so left and bottom buttons are never active at the same time.
   let className = getClassToActivate();
   let scientificButtons = document.querySelectorAll(className);
-  let displayProperty = getNewDisplayProperty(enable);
   for (let i = 0; i < scientificButtons.length; i++) {
-    scientificButtons[i].style.display = displayProperty;
+    scientificButtons[i].style.display = "block";
   }
 }
 
@@ -27,9 +40,13 @@ function getClassToActivate() {
   }
 }
 
-function getNewDisplayProperty(enable) {
-  if (enable) {
-    return "block";
+function disableScientificButtons() {
+  setDisplayToNone(document.querySelectorAll(".scientific-btn-left"));
+  setDisplayToNone(document.querySelectorAll(".scientific-btn-bottom"));
+}
+
+function setDisplayToNone(buttons) {
+  for (let i = 0; i < buttons.length; i++) {
+    buttons[i].style.display = "none";
   }
-  return "none";
 }
